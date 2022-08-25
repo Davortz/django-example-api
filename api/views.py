00d8dca3faf49413930b4ad4d6d791460bdff08f
Davortz <davortix@gmail.com>
2022-08-24 22:35:35 -0500

    add() inicio de modelos y conecxion a DB

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.views import View
+from .models import Company
+from django.http.response import JsonResponse
 # Create your views here.
+
+
+class CompanyView(View):
+
+    def get(self, request):
+        companies = list(Company.objects.values())
+        if len(companies) > 0:
+            datos= {
+                'messsage':'Succes','companies':companies
+            }
+        else:
+            datos ={
+                'message':'Companies not found'
+            }
+        return JsonResponse(datos)
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
--- proyecto_api/settings.py
+++ proyecto_api/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'proyecto_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'NAME': 'django_api',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -104,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
